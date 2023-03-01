f80852067b38caec1f3ca2bef5624927f9102b83
SkundPratap <pratapskund@gmail.com>
2023-03-01 20:03:35 +0530

    Initial commit

---
+++ Work/EY/my-app/src/App.js
@@ -0,0 +1,13 @@
+// use 'rafce' to create a react arrow function component in an instant.  
+ import React from 'react'
+ 
+ const App = () => {
+     return (
+         <div>
+             E-Commerce app.
+         </div>
+     )
+ }
+ 
+ export default App
+ 
---
+++ Work/EY/my-app/src/index.js
@@ -0,0 +1,6 @@
+import react from 'react'; 
+import ReactDOM from 'react-dom'; 
+import App from './App'; 
+
+// this is to render the app inside the root element inside the HTML page.
+ReactDOM.render(<App/>, document.getElementById('root')); 
